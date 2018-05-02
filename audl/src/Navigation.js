b88fc84e0ba4b80ef42ced03f134f853add48a54
Jiří Zajíc <jiri.zajic@mensa.cz>
2018-05-02 14:08:18 -0700

    AUDL - improved navigation (.active)

--- audl/src/Navigation.js
+++ audl/src/Navigation.js
@@ -1,10 +1,44 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
+import cx from 'classnames';
 import './App.css';
 
 class Navigation extends Component {
   render() {
-    const isActive = false && 'active'; // TODO
+    const hash = window.location.hash.slice(1);
+    const menuItems = [
+      { title: 'Getting Started', section: true, path: '/' },
+      { title: 'Solutions', section: true },
+      { title: 'Default boilerplate' },
+      { title: 'Document Managenent', path: '/document-management' },
+      { title: 'Traffic Analysis' },
+
+      { title: 'Organisms', section: true, path: '/empty' },
+      { title: 'Navigation: top', path: '/empty' },
+      { title: 'Navigation: left', path: '/empty' },
+      { title: 'Navigation: homepage', path: '/organisms/navigation-homepage' },
+      { title: 'Embedded Analytical Designer', path: '/empty' },
+      { title: 'Side-by-Side Comparison', path: '/organisms/side-by-side-comparison' },
+
+      { title: 'Molecules', section: true, path: '/empty' },
+      { title: 'KPI block', path: '/empty' },
+      { title: 'BarChart', path: '/empty' },
+      { title: 'ColumnChart', path: '/empty' },
+      { title: 'LineChart', path: '/empty' },
+      { title: 'Filter - Date', path: '/molecules/filter-date' },
+      { title: 'Filter - Attribute', path: '/empty' },
+
+      { title: 'Atoms', section: true, path: '/empty' },
+      { title: 'Label', path: '/empty' },
+      { title: 'Kpi', path: '/atoms/kpi' },
+      { title: 'Legend', path: '/empty' },
+
+      { title: 'Interactions', section: true, path: '/empty' },
+      { title: 'Open in AD', path: '/empty' },
+      { title: 'Visual filtersKpi', path: '/atoms/kpi' },
+      { title: 'Exporting', path: '/empty' },
+      { title: 'Eventing', path: '/empty' },
+    ];
 
     return (
       <div className="Navigation">
@@ -13,38 +47,25 @@ class Navigation extends Component {
         </div>
         <div className="LeftNavBlock">
           <ul className="ul">
-            <li><Link className={`MenuItem Section ${isActive}`} to="/">Getting started</Link></li>
-            <li><span className="MenuItem Section">Solutions</span></li>
-            <li><span className="MenuItem Child">Default boilerplate</span></li>
-            <li><Link className={`MenuItem Child ${isActive}`} to="/document-management">Document Management</Link></li>
-            <li><span className="MenuItem Child">Traffic Analysis</span></li>
+            {
+              menuItems.map((menuItem) => {
+                const Tag = menuItem.path ? Link : 'span';
+                const props = {
+                  to: menuItem.path,
+                };
+                const className = cx('MenuItem', {
+                  Section: menuItem.section,
+                  Child: !menuItem.section,
+                  active: menuItem.path === hash && hash !== '/empty',
+                });
 
-            <li><Link className={`MenuItem Section ${isActive}`} to="/empty">Organisms</Link></li>
-            <li><Link className={`MenuItem Child ${isActive}`} to="/empty">Navigation: top</Link></li>
-            <li><Link className={`MenuItem Child ${isActive}`} to="/empty">Navigation: left</Link></li>
-            <li><Link className={`MenuItem Child ${isActive}`} to="/organisms/navigation-homepage">Navigation: Homepage</Link></li>
-            <li><Link className={`MenuItem Child ${isActive}`} to="/empty">Embedded Analytical Designer</Link></li>
-
-            <li><Link className={`MenuItem Child ${isActive}`} to="/organisms/side-by-side-comparison">Side-by-side comparison</Link></li>
-            <li><Link className={`MenuItem Section ${isActive}`} to="/empty">Molecules</Link></li>
-            <li><Link className={`MenuItem Child ${isActive}`} to="/empty">KPI block</Link></li>
-            <li><Link className={`MenuItem Child ${isActive}`} to="/empty">Barchart</Link></li>
-            <li><Link className={`MenuItem Child ${isActive}`} to="/empty">Columnchart</Link></li>
-
-            <li><Link className={`MenuItem Child ${isActive}`} to="/empty">Linechart</Link></li>
-            <li><Link className={`MenuItem Child ${isActive}`} to="/molecules/filter-date">Filter - Date</Link></li>
-            <li><Link className={`MenuItem Child ${isActive}`} to="/empty">Filter - Attribute</Link></li>
-
-            <li><Link className={`MenuItem Section ${isActive}`} to="/empty">Atoms</Link></li>
-            <li><Link className={`MenuItem Child ${isActive}`} to="/empty">Label</Link></li>
-            <li><Link className={`MenuItem Child ${isActive}`} to="/atoms/kpi">KPI</Link></li>
-            <li><Link className={`MenuItem Child ${isActive}`} to="/empty">Legend</Link></li>
-
-            <li><Link className={`MenuItem Section ${isActive}`} to="/empty">Interactions</Link></li>
-            <li><Link className={`MenuItem Child ${isActive}`} to="/empty">Open in AD</Link></li>
-            <li><Link className={`MenuItem Child ${isActive}`} to="/empty">Visual filters</Link></li>
-            <li><Link className={`MenuItem Child ${isActive}`} to="/empty">Exporting</Link></li>
-            <li><Link className={`MenuItem Child ${isActive}`} to="/empty">Eventing</Link></li>
+                return (
+                  <li>
+                    <Tag className={className} {...props}>{menuItem.title}</Tag>
+                  </li>
+                );
+              })
+            }
           </ul>
         </div>
       </div>
