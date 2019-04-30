2a9afbca2f14149d64c09aca34eafe5fed73739a
Jiří Zajíc <jiri.zajic@mensa.cz>
2019-04-30 09:51:57 +0200

    gd-ui-boilerplate - fl colors

--- gd-ui-boilerplate/src/App.js
+++ gd-ui-boilerplate/src/App.js
@@ -107,13 +107,72 @@ class App extends Component {
           <Visualization
             {...config}
             filterGroup={FG_MAIN}
+            identifier={C.visualization('$ Ab Cost by Category (Pie by Jiri)')}
+            config={{
+              colorPalette: [{
+                guid: '01',
+                fill: { r: 255, g: 255, b: 60 }
+              }, {
+                guid: '02',
+                fill: { r: 170, g: 108, b: 57 }
+              }, {
+                guid: '03',
+                fill: { r: 34, g: 102, b: 102 }
+              }, {
+                guid: '04',
+                fill: { r: 45, g: 136, b: 45 }
+              }, {
+                guid: '05',
+                fill: { r: 212, g: 106, b: 106 }
+              }, {
+                guid: '06',
+                fill: { r: 212, g: 154, b: 106 }
+              }, {
+                guid: '07',
+                fill: { r: 64, g: 127, b: 127 }
+              }, {
+                guid: '08',
+                fill: { r: 85, g: 170, b: 85 }
+              }, {
+                guid: '09',
+                fill: { r: 85, g: 0, b: 0 }
+              }],
+              colorMapping: [{
+                  predicate: (headerItem) =>
+                    headerItem.attributeHeaderItem && headerItem.attributeHeaderItem.name === 'Admin',
+                  color: {
+                    type: 'guid',
+                    value: '01'
+                  }
+              }, {
+                  predicate: (headerItem) =>
+                    headerItem.attributeHeaderItem && headerItem.attributeHeaderItem.name === 'App-Service',
+                  color: {
+                    type: 'guid',
+                    value: '02'
+                  }
+              }, {
+                  predicate: (headerItem) =>
+                    headerItem.attributeHeaderItem && headerItem.attributeHeaderItem.uri === '/gdc/md/zuyu5r42fr85ekgo982t8px41we1baef/obj/234/elements?id=15',
+                  color: {
+                    type: 'guid',
+                    value: '03'
+                  }
+              }]
+            }}
+          />
+        </div>
+        <div style={{ height: 400 }}>
+          <Visualization
+            {...config}
+            filterGroup={FG_MAIN}
             config={{
               legend: {
                 enabled: true,
                 position: 'right'
               }
             }}
-            identifier={C.visualization('Category/RT Costs (by Jiri)')}
+            identifier={C.visualization('Category/RT Costs (Bar by Jiri)')}
           />
         </div>
         <div style={{ height: 400 }}>
