7b4f18c4fa0796094a632b492d1cf47bf562dd2f
Jiří Zajíc <jiri.zajic@mensa.cz>
2020-10-06 16:11:23 -0700

    yarn.lock

--- gduiv8-launch-test/src/Snippet01/index.js
+++ gduiv8-launch-test/src/Snippet01/index.js
@@ -2,18 +2,13 @@ import React from "react";
 import { Headline } from "@gooddata/sdk-ui-charts";
 import * as Ldm from "../ldm";
 import Hint from "./Hint";
-import backend from "../backend";
 
 export default () => (
   <>
     <h1># of Orders</h1>
 
     {/* Try editing the component below 👇 */}
-    <Headline
-      primaryMeasure={Ldm.NrOfOrders}
-      backend={backend}
-      workspace="psk46untnn2vhzs5bokdav3bu2puf6s6"
-    />
+    <Headline primaryMeasure={Ldm.NrOfOrders} />
 
     <Hint />
   </>
